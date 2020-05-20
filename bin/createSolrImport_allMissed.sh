3384475a17bf9e7dfd498399ac7abb78a50ff49b
Paul Borchert <Paul.Borchert@gbv.de>
2020-05-20 14:50:22 +0200

    add handle gziped epustalogs

--- bin/createSolrImport_allMissed.sh
+++ bin/createSolrImport_allMissed.sh
@@ -8,13 +8,26 @@ else
     exit 1
 fi 
 
-for filename in $epustaLogs/*.log; do
-    basename="$(basename $filename .log)";
-    destfile=$basename.json
-    if [ ! -f "$solrImports/$destfile" ]; then
-        echo "Processing: $filename -> $destfile"
-        $epustaServerBin/createSolrImport.php --file=$filename --level=PROD > $solrImports/$destfile
-    else
-        echo "$filename allready parsed."
+for filename in $epustaLogs/*; do
+    if [ -f $filename ]; then
+        command="$epustaServerBin/createSolrImport.php --file=$filename --level=PROD"
+        if [ ${filename: -3} == ".gz" ]; then
+            basename="$(basename $filename .log.gz)";
+            filename2=${filename: 0 : -3};
+        elif [ ${filename: -4} == ".log" ]; then
+            basename="$(basename $filename .log)";
+            filename2=$filename;
+        else
+            echo "Error: Wrong Filenamepatern - skip processing."
+        fi
+        destfile=$basename.json
+        if [ ! -f "$solrImports/$destfile" ]; then
+            echo "Processing: $filename -> $destfile"
+            if [ ${filename: -3} == ".gz" ]; then gzip -d $filename; fi
+            $epustaServerBin/createSolrImport.php --file=$filename2 --level=PROD > $solrImports/$destfile
+            if [ ${filename: -3} == ".gz" ]; then gzip $filename2; fi
+        else
+            echo "$filename allready parsed."
+        fi
     fi
 done
