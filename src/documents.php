d1f4eee3d3053e1a4847de2023825d7abf55319e
Paul Borchert <paul.borchert@gbv.de>
2023-07-20 14:04:47 +0200

    add missing core to url

--- src/documents.php
+++ src/documents.php
@@ -2,7 +2,7 @@
 
 function getDocuments($start_date, $end_date,$tags, $start, $limit) {
     global $config;
-    $solrUrl=$config['solrUrl'].'/query';
+    $solrUrl=$config['solrUrl'].$config['solrCore'].'/query';
 
     $query='q=dateTime:['.$start_date.'T00:00:00Z TO '.$end_date.'T23:59:59Z]';
     foreach ($tags as $tag) {
